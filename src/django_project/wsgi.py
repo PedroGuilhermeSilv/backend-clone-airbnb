42e889de3f8672787caedbdf01f9917be9dd9476
guilherme <vinespedro55@gmail.com>
2024-12-07 02:05:13 -0300

    fix: update wsgi

--- src/django_project/wsgi.py
+++ src/django_project/wsgi.py
@@ -13,7 +13,7 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault(
     "DJANGO_SETTINGS_MODULE",
-    f'src.django_project.settings{os.getenv("DJANGO_ENV", "dev")}',
+    f'src.django_project.settings.{os.getenv("DJANGO_ENV", "dev")}',
 )
 
 application = get_wsgi_application()
